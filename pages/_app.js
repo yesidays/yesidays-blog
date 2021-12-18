98330744f12c208bbac34af9a9bc90663457da38
Yesi Díaz <silvercorp@gmail.com>
2021-12-17 19:47:34 -0600

    cross

--- pages/_app.js
+++ pages/_app.js
@@ -64,10 +64,8 @@ function MyApp({ Component, pageProps }) {
             }}
           />
 
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1435995620378131"
-          crossOrigin="anonymous"></script>
-      <ins className="adsbygoogle"
-          style="display:block; text-align:center;"
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1435995620378131" crossOrigin="anonymous"></script>
+      <ins className="adsbygoogle" style="display:block; text-align:center;"
           data-ad-layout="in-article"
           data-ad-format="fluid"
           data-ad-client="ca-pub-1435995620378131"
